260de45ee3740950f1bac2d789c903723b24fde9
Gabriel Chartier <gabriel@chartier.link>
2025-06-25 11:12:25 -0500

    update source regex check to handle patterns like snek.fun

--- src/util/archives.ts
+++ src/util/archives.ts
@@ -190,7 +190,7 @@ async function getSourceDetailsFromArchive(
 
     // Build the list of sources in the archive
     const archiveSources = messageFiles.reduce((acc, sourceMessage) => {
-      const match = sourceMessage.fileName.match(/-([\w]+?)(?:\.tick_|-\d{4}-\d{2}-\d{2}T)/);
+      const match = sourceMessage.fileName.match(/-([\w.]+?)(?:\.tick_|-\d{4}-\d{2}-\d{2}T)/);
       if (!match)
         throw new Error(
           `Error retrieving source name from file name for network ${network.name}, file: ${sourceMessage.fileName}`
